341f3711d9c7e763f0d3d94d6c76f89551251117
matheusraffu <matheus.martinez@sptech.school>
2025-09-19 16:22:50 -0300

    Sure

--- src/main/java/school/sptech/prova_ac1/UsuarioController.java
+++ src/main/java/school/sptech/prova_ac1/UsuarioController.java
@@ -1,36 +1,91 @@
 package school.sptech.prova_ac1;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
+    List <Usuario> usuarios = new ArrayList<>();
+
+
+    @GetMapping
     public ResponseEntity<List<Usuario>> buscarTodos() {
-        return ResponseEntity.internalServerError().build();
+        if (usuarios.isEmpty()) {
+            return ResponseEntity.noContent().build(); // 204
+        }
+        return ResponseEntity.ok(usuarios); // 200
     }
 
-    public ResponseEntity<Usuario> criar(Usuario usuario) {
-        return ResponseEntity.internalServerError().build();
+
+    @PostMapping
+    public ResponseEntity<Usuario> criar(@RequestBody Usuario usuario) {
+        for (Usuario u : usuarios) {
+            if (usuario.getCpf().equals(u.getCpf()) || usuario.getEmail().equals(u.getEmail())) {
+                return ResponseEntity.status(409).build();
+            }
+        }
+
+        usuarios.add(usuario);
+        return ResponseEntity.status(201).body(usuario); // 201 Created
     }
 
-    public ResponseEntity<Usuario> buscarPorId(Integer id) {
-        return ResponseEntity.internalServerError().build();
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarPorId(@PathVariable Integer id) {
+        for (Usuario u : usuarios) {
+            if (id.equals(u.getId())) {
+                return ResponseEntity.ok(u);
+            }
+        }
+        return ResponseEntity.notFound().build();
     }
 
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletar(Integer id) {
+        for (Usuario u : usuarios) {
+            if (id.equals(u.getId())) {
+               usuarios.remove(u);
+            }
+        }
         return ResponseEntity.internalServerError().build();
     }
 
-    public ResponseEntity<List<Usuario>> buscarPorDataNascimento(LocalDate nascimento) {
-        return ResponseEntity.internalServerError().build();
+
+    @GetMapping("/filtro-data")
+    public ResponseEntity<List<Usuario>> buscarPorDataNascimento(@RequestParam LocalDate nascimento) {
+        List<Usuario> filtrados = new ArrayList<>();
+        for (Usuario u : usuarios) {
+            if (u.getDataNascimento().equals(u.getDataNascimento())) {
+                filtrados.add(u);
+            }
+        }
+        if (filtrados.isEmpty()) {
+            return ResponseEntity.noContent().build(); // 204
+        }
+        return ResponseEntity.ok(filtrados); // 200
     }
 
-    public ResponseEntity<Usuario> atualizar(
-            Integer id,
-            Usuario usuario
-    ) {
-        return ResponseEntity.internalServerError().build();
-    }
-}
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizar(@PathVariable Integer id, @RequestBody Usuario usuario) {
+        for (int i = 0; i < usuarios.size(); i++) {
+            if (id.equals(usuarios.get(i).getId())) {
+
+                for (Usuario u : usuarios) {
+                    if (!u.getId().equals(id) &&
+                            (u.getCpf().equals(usuario.getCpf()) || u.getEmail().equals(usuario.getEmail()))) {
+                        return ResponseEntity.status(409).build();
+                    }
+                }
+
+                return ResponseEntity.ok(usuario); // 200
+            }
+        }
+        return ResponseEntity.notFound().build(); // 404
+    }}
